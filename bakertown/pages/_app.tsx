6e028aa4471d0f98938913265daab339269b1804
NiNinanana <yeen00123@gmail.com>
2021-12-27 15:07:06 +0900

    [update] 파이어베이스 _app.tsx 생성

--- bakertown/pages/_app.tsx
+++ bakertown/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAyxVLveHqFlLxF0fSjDmsrTiuYOma1VI0",
+  authDomain: "baker-town.firebaseapp.com",
+  projectId: "baker-town",
+  storageBucket: "baker-town.appspot.com",
+  messagingSenderId: "32593744544",
+  appId: "1:32593744544:web:6d8555c1988dd947baafc5",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
