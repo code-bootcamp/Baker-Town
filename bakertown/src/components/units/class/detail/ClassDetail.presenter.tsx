22a45719ec9f1ecbbc1e9fbfc31e69ddd1aafd6c
shinnr93 <shinnr93@gmail.com>
2021-12-29 13:18:03 +0900

    [modify]중간 업로드2

--- bakertown/src/components/units/class/detail/ClassDetail.presenter.tsx
+++ bakertown/src/components/units/class/detail/ClassDetail.presenter.tsx
@@ -7,9 +7,20 @@ const ClassDetailPresenter = (props: IClassDetailPresenterProps) => {
       <S.WholeWrapper>
         <S.IntroWrapper>
           <S.ClassIntro>
-            <S.ClassTitle>
-              <div>클래스 소개글</div>
-            </S.ClassTitle>
+            <S.ClassTitle>내 입맛을 돋워줄 나만의 마카롱 만들기</S.ClassTitle>
+
+            <S.SubComponentWrapper>
+              <S.SubTheme1>
+                <S.ClassStarRate>별점</S.ClassStarRate>
+                <S.CategoryTag>송파구•마카롱</S.CategoryTag>
+              </S.SubTheme1>
+
+              <S.SubTheme2>
+                <S.DibsOnClass>찜하기</S.DibsOnClass>
+                <S.ClassShare>공유하기</S.ClassShare>
+              </S.SubTheme2>
+            </S.SubComponentWrapper>
+
             <S.ClassImage>
               <div>파티셰가 넣는 소개 이미지</div>
             </S.ClassImage>
@@ -17,37 +28,44 @@ const ClassDetailPresenter = (props: IClassDetailPresenterProps) => {
 
           <S.PatissierIntro>
             <S.ClassRemarksTitle>
-              <div>파티셰가 소개하는 클래스 한줄 소개</div>
+              ㅇㅇㅇ님과 함께하는 마카롱 클래스
               <div>{props.myClass.className}</div>
             </S.ClassRemarksTitle>
-            <S.ClassRemarksDetail></S.ClassRemarksDetail>
+            <S.ClassRemarksDetail>가성비로 소문난 바로 그 클래스 맞습니다</S.ClassRemarksDetail>
 
-            <S.ProgramIntro>
+            <S.ProgramIntro>프로그램
+              <S.ProgramGuide>
               <div>
                 <div>프로그램 소개글</div>
                 <div>{props.myClass.contents}</div>
               </div>
+              </S.ProgramGuide>
             </S.ProgramIntro>
             <S.ProgramImage>
               <div>파티셰가 넣는 예시 이미지</div>
             </S.ProgramImage>
-            <S.PatissierRemarks>
+
+            <S.PatissierInfo>
+              <S.PatissierPhoto>사진자리</S.PatissierPhoto>
+              <S.PatissierName>홍길동 파티셰</S.PatissierName>
+              <S.ContactPatissier>연락하기</S.ContactPatissier>
               <div>파티셰 소개글</div>
               <div>{props.myClass.patissier}</div>
-            </S.PatissierRemarks>
+            </S.PatissierInfo>
           </S.PatissierIntro>
 
-          <S.ClassLocation>
+          <S.ClassLocationInfo>
+            <S.LocationInfo></S.LocationInfo>
             <div>위치정보</div>
             <div>{props.myClass.address}</div>
-          </S.ClassLocation>
+          </S.ClassLocationInfo>
+
+          <S.ClassReviewInfo></S.ClassReviewInfo>
           <S.ClassReview>실제 수강생 후기</S.ClassReview>
         </S.IntroWrapper>
 
         <S.ScheduleWrapper>
-          <S.ClassSchedule>
-            <div>클래스 일정</div>
-          </S.ClassSchedule>
+          <S.ClassSchedule></S.ClassSchedule>
         </S.ScheduleWrapper>
       </S.WholeWrapper>
     </>
--- bakertown/src/components/units/class/detail/ClassDetail.styles.ts
+++ bakertown/src/components/units/class/detail/ClassDetail.styles.ts
@@ -4,6 +4,7 @@ export const WholeWrapper = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
+  padding-top: 70px;
 `;
 
 export const IntroWrapper = styled.div`
@@ -14,8 +15,6 @@ export const IntroWrapper = styled.div`
   /* background-color: green; */
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `;
 
 export const ClassIntro = styled.div`
@@ -28,9 +27,44 @@ export const ClassIntro = styled.div`
 export const ClassTitle = styled.div`
   /* height: 60px; */
   /* justify-content: center; */
-  width: 865px;
+  height: 44px;
+  width: 540px;
   border: 1px solid gray;
   padding-bottom: 90px;
+  font-size: 32px;
+  font-weight: bold;
+`;
+
+export const SubComponentWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const SubTheme1 = styled.div`
+  display: flex;
+  padding-right: 255px;
+`;
+
+export const ClassStarRate = styled.div`
+  padding-right: 30px;
+`;
+
+export const CategoryTag = styled.div``;
+export const SubTheme2 = styled.div`
+  display: flex;
+`;
+
+export const DibsOnClass = styled.div`
+  width: 140px;
+  height: 50px;
+  border-radius: 5px;
+  background-color: #eee;
+  margin-right: 8px;
+`;
+export const ClassShare = styled.div`
+  width: 140px;
+  height: 50px;
+  border-radius: 5px;
+  background-color: #eee;
 `;
 
 export const ClassImage = styled.div`
@@ -38,32 +72,67 @@ export const ClassImage = styled.div`
   width: 865px;
   height: 324px;
   border: 1px solid gray;
+  background-color: #c4c4c4;
 `;
 
 export const PatissierIntro = styled.div``;
 
 export const ClassRemarksTitle = styled.div`
-  height: 95px;
-  background-color: yellow;
+  width: 599px;
+  height: 44px;
+  margin-top: 50px;
   border: 1px solid gray;
-  padding-top: 50px;
+  font-family: ;
+  font-size: 20px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #1e1e1e;
 `;
 
-export const ClassRemarksDetail = styled.div``;
+export const ClassRemarksDetail = styled.div`
+width: 477;
+height: 33px;
+padding-top: 20px;
+font-size: 24px;
+font-weight: normal;
+font-stretch: normal;
+line-height: normal;
+letter-spacing: normal;
+text-align: left;
+color: #1e1e1e;
+`;
 
 export const ProgramIntro = styled.div`
   height: 300px;
+  padding-top: 80px;
 `;
+
+export const ProgramGuide = styled.div`
+padding-top: 80px;
+`
 
 export const ProgramImage = styled.div``;
 
-export const PatissierRemarks = styled.div`
+export const PatissierInfo = styled.div`
   height: 80px;
 `;
 
-export const ClassLocation = styled.div`
-  height: 200px;
+export const PatissierPhoto = styled.div``;
+export const PatissierName = styled.div``;
+
+export const ContactPatissier = styled.div``;
+
+export const ClassLocationInfo = styled.div`
+  /* height: 200px; */
 `;
+
+export const LocationInfo = styled.div``;
+
+export const ClassReviewInfo = styled.div``;
 
 export const ClassReview = styled.div`
   height: 250px;
