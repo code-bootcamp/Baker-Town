ba2d61479db665b2c7ba01ddb2cf0d26dc4ea3a9
NiNinanana <yeen00123@gmail.com>
2021-12-31 11:03:03 +0900

    [update] class list UI 만들기, class list 파이어베이스 불러오기

--- bakertown/src/components/units/class/list/ClassList.container.tsx
+++ bakertown/src/components/units/class/list/ClassList.container.tsx
@@ -1,9 +1,31 @@
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "@firebase/firestore";
+import { useEffect, useState } from "react";
+import { firebaseApp } from "../../../../../pages/_app";
 import ClassListPresenter from "./ClassList.presenter";
 
 const ClassListContainer = () => {
+  const [recent, setRecent] = useState([]);
+
+  useEffect(async () => {
+    const recent = query(
+      collection(getFirestore(firebaseApp), "class"),
+      where("createdAt", "!=", "")
+    );
+    let result = await getDocs(recent);
+    let docs = result.docs.map((el) => el.data());
+    console.log(docs);
+    setRecent(docs);
+  });
+
   return (
     <>
-      <ClassListPresenter />
+      <ClassListPresenter recent={recent} />
     </>
   );
 };
