5c48ac4cee6261a501b1c0adc2ce6b0e17877a19
shinnr93 <shinnr93@gmail.com>
2022-01-05 10:46:05 +0900

    [modify]파티셰 클래스등록페이지UI 수정중(1/2)

--- bakertown/src/components/units/dashboard/class/write/DashBoardClassWrite.styles.ts
+++ bakertown/src/components/units/dashboard/class/write/DashBoardClassWrite.styles.ts
@@ -1,11 +1,94 @@
 import styled from "@emotion/styled";
+import { ECDH } from "crypto";
 
 export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  margin-top: 30px;
+  margin-left: 90px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+
   /* background-color: salmon; */
+`;
+
+export const Title = styled.div`
+  margin-bottom: 10px;
+  font-size: 32px;
+  font-weight: bold;
+`;
+
+export const ClassNameWrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ClassNameTitle = styled.div``;
+
+export const ClassName = styled.input`
+  width: 900px;
+`;
+
+export const ClassPatissierWrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ClassPatissierTitle = styled.div``;
+
+export const ClassPatissier = styled.input`
+  width: 900px;
+`;
+
+export const ClassCategoryWrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ClassCategoryTitle = styled.div``;
+
+export const ClassCategorySelect = styled.select`
+  width: 450px;
+`;
+
+export const ClassCategoryOption = styled.option``;
+
+export const ClassRemarksWrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ClassRemarksTitle = styled.div``;
+
+export const ClassRemarks = styled.input`
+  width: 900px;
+`;
+
+export const ClassContentsWrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ClassContentsTitle = styled.div``;
+
+export const ClassContents = styled.input`
+  width: 900px;
+`;
+
+export const ClassPriceWrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ClassPriceTitle = styled.div``;
+
+export const ClassPrice = styled.input`
+  width: 900px;
 `;
