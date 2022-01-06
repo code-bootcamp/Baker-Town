6011b546dff425ec0675f33271dc0c9457c7b5ca
chanmi94 <83502510+chanmi94@users.noreply.github.com>
2022-01-06 20:16:41 +0900

    [modfiy]결제UI 수정

---
+++ bakertown/src/components/commons/payment/MyPoint.styles.ts
@@ -0,0 +1,25 @@
+import { ButtonBase } from "@mui/material";
+
+import styled from "@emotion/styled";
+
+export const MyPoint = styled.div`
+  margin: 9px 47px 10px 30px;
+  font-size: 27px;
+  text-align: center;
+  color: #ff4d12;
+`;
+
+// export const ChargeButton = styled(ButtonBase)`
+//   margin: 9px 87px 10px 30px;
+//   font-size: 30px;
+//   text-align: center;
+//   bac
+// `;
+
+export const ChargeButton = styled.button`
+  margin: 9px 47px 10px 60px;
+  font-size: 20px;
+  text-align: center;
+  color: black;
+  background: #fbf5ef;
+`;
