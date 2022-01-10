ced1e8820514f4c413c4be5f58863d5c14d924eb
NiNinanana <yeen00123@gmail.com>
2022-01-10 13:02:21 +0900

    [update] 마이페이지 아이템 부분 기능 넣기, 아이템 리뷰 작성 진행중

--- bakertown/src/components/units/myPage/item/basket/Basket.presenter.tsx
+++ bakertown/src/components/units/myPage/item/basket/Basket.presenter.tsx
@@ -2,10 +2,7 @@ import * as S from "./Basket.styles";
 import { v4 as uuidv4 } from "uuid";
 import SideNavigationContainer from "../../sideNav/SideNavigation.container";
 
-const BasketPresenter = () => {
-  const myClass = ["참여 예정", "참여 완료", "나의 후기", "찜 목록"];
-  const myItem = ["주문 내역", "장바구니", "리뷰"];
-
+const BasketPresenter = (props) => {
   return (
     <>
       <S.Wrapper>
@@ -16,15 +13,15 @@ const BasketPresenter = () => {
           </S.BasketTitle>
           <S.Basket>
             <S.BasketContents>
-              {new Array(4).fill(1).map((el) => (
+              {props.userResult?.itemHeart?.map((el) => (
                 <S.ItemWrapper key={uuidv4()}>
                   <S.BasketCheckBoxContainer>
                     <S.BasketCheckBox></S.BasketCheckBox>
                   </S.BasketCheckBoxContainer>
                   <S.ItemImage />
                   <S.ItemInfo>
-                    <S.ItemName>조리도구 세트</S.ItemName>
-                    <S.ItemPrice>원</S.ItemPrice>
+                    <S.ItemName>{el.itemName}</S.ItemName>
+                    <S.ItemPrice>{el.price}원</S.ItemPrice>
                   </S.ItemInfo>
                 </S.ItemWrapper>
               ))}
--- bakertown/src/components/units/myPage/item/review/Review.container.tsx
+++ bakertown/src/components/units/myPage/item/review/Review.container.tsx
@@ -1,7 +1,30 @@
-import ReviewPresenter from "./Review.presenter"
+import ReviewPresenter from "./Review.presenter";
+import { doc, getDoc, getFirestore } from "@firebase/firestore";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { firebaseApp, useAuth } from "../../../../../../pages/_app";
 
 const ReviewContainer = () => {
-    return <ReviewPresenter />
-}
+  const router = useRouter();
+  const currentUser = useAuth();
+  const [myUser, setMyUser] = useState({
+    name: "로딩중입니다",
+  });
 
-export default ReviewContainer
+  useEffect(async () => {
+    if (myUser?.name === "로딩중입니다") {
+      if (!currentUser) return;
+      const userQuery = doc(
+        getFirestore(firebaseApp),
+        "users",
+        currentUser?.email
+      );
+      const userResult = await getDoc(userQuery);
+      setMyUser(userResult.data());
+      console.log("aaa", userResult.data());
+    }
+  });
+  return <ReviewPresenter userResult={myUser} />;
+};
+
+export default ReviewContainer;
