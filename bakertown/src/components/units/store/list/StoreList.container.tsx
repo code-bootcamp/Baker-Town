bf8f81ff5fdd644389a1a5ff906b7ceb882381e9
shinnr93 <shinnr93@gmail.com>
2022-01-10 20:07:40 +0900

    [modify]클래스리스트, [modify]dashboard 아이템등록 ui 틀잡기시작

--- bakertown/src/components/units/store/list/StoreList.container.tsx
+++ bakertown/src/components/units/store/list/StoreList.container.tsx
@@ -1,28 +1,51 @@
-import { collection, query, getFirestore, where, orderBy, limit, getDocs, } from "firebase/firestore";
-import { firebaseApp } from "../../../../../pages/_app"
-import { useEffect } from "react";
+import {
+  collection,
+  query,
+  getFirestore,
+  where,
+  orderBy,
+  limit,
+  getDocs,
+} from "firebase/firestore";
+import { firebaseApp } from "../../../../../pages/_app";
+import { useEffect, useState } from "react";
 import StoreListPresenter from "./StoreList.presenter";
+import { useRouter } from "next/router";
 
 const StoreListContainer = () => {
+  const router = useRouter();
+  const [recent, setRecent] = useState([]);
+  const [option, setOption] = useState(0);
+  const categoryName = router.query.categoryName;
 
   useEffect(async () => {
-      // 전체 클래스
-      const first = query(
-        collection(getFirestore(firebaseApp), "item"),
-        where("createdAt", "!=", ""), // 필터
-        orderBy("createdAt", "desc"), // 정렬
-        limit(12) // 데이터 불러오는 개수 제한
-      );
-      const firstResult = await getDocs(first);
-      console.log(
-        "찬밍",
-        firstResult.docs.map((el) => el.ref.firestore.app)
-      );
-      setRecent(firstResult.docs.map((el) => el.data()));
-    })
-  );
+    // 전체 클래스
+    const first = query(
+      collection(getFirestore(firebaseApp), "item"),
+      where("createdAt", "!=", ""), // 필터
+      orderBy("createdAt", "desc"), // 정렬
+      limit(12) // 데이터 불러오는 개수 제한
+    );
+    const firstResult = await getDocs(first);
+    setRecent(firstResult.docs.map((el) => el.data()));
+    console.log(recent);
+  }, []);
 
-  return <StoreListPresenter />;
+  const onClickSideButton = (el: string) => () => {
+    router.push(`/store/category/${el}`);
+  };
+
+  // const onClickOption = (event) => {
+  //   setOption(event.target.id);
+  // };
+
+  return (
+    <StoreListPresenter
+      recent={recent}
+      onClickSideButton={onClickSideButton}
+      // onClickOption={onClickOption}
+    />
+  );
 };
 
 export default StoreListContainer;
